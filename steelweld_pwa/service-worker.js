d27acf4c59be1fe4546fb2fdd7177918b6652556
christianmamaniveliz-dot <christianmamaniveliz@gmail.com>
2025-11-27 17:26:21 -0400

    Delete steelweld_pwa directory

--- steelweld_pwa/service-worker.js
+++
@@ -1,9 +0,0 @@
-
-self.addEventListener('install', function(e){
-  e.waitUntil(caches.open('steelweld-demo-v1').then(function(cache){
-    return cache.addAll(['./','./index.html','./style.css','./app.js']);
-  }));
-});
-self.addEventListener('fetch', function(e){
-  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
-});
